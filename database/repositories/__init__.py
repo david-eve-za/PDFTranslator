b8f09a2e8417dfe6a2170b7620d5b54b1d85711e
David Gonzalez <davidgonzalez@Davids-Mac-Studio.local>
2026-03-21 13:35:18 -0500

    feat: add FantasyTermRepository

--- database/repositories/__init__.py
+++ database/repositories/__init__.py
@@ -3,6 +3,7 @@ from database.repositories.base import BaseRepository
 from database.repositories.book_repository import BookRepository
 from database.repositories.chapter_repository import ChapterRepository
 from database.repositories.entity_blacklist_repository import EntityBlacklistRepository
+from database.repositories.fantasy_term_repository import FantasyTermRepository
 from database.repositories.glossary_repository import GlossaryRepository
 from database.repositories.volume_repository import VolumeRepository
 
@@ -11,6 +12,7 @@ __all__ = [
     "BookRepository",
     "ChapterRepository",
     "EntityBlacklistRepository",
+    "FantasyTermRepository",
     "GlossaryRepository",
     "VolumeRepository",
 ]
